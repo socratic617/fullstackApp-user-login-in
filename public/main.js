9e38a4ab27af89bc232d2df177a1b74015d9a650
socratic617 <erik.solis02135@gmail.com>
2023-11-17 21:22:49 -0500

    remove logs

--- public/main.js
+++ public/main.js
@@ -1,20 +1,11 @@
 var trash = document.getElementsByClassName("trash");
 var favorite = document.getElementsByClassName("favorite")
 
-// document.querySelector('#create').addEventListener('click', addUserDetails)
-
-// function addUserDetails(){
-
-// }
-
 Array.from(favorite).forEach(function(element) {
   console.log("adding favorite listener too : ", element);
 
     element.addEventListener('click', function(){
-      console.log('FAVORITE UPDATE : ')
-      console.log("this.id : " , this.id )
-      console.log("before my fetch loggedInUserId :")
-      console.log( document.querySelector('.loggedInUserId'))
+
       fetch('recipes', {
         method: 'put',
         headers: {
@@ -34,7 +25,6 @@ Array.from(favorite).forEach(function(element) {
 console.log("trash elements : ", trash)
 
 Array.from(trash).forEach(function(element) {
-  console.log("adding delete listner too : ", element);
 
     element.addEventListener('click', function(){
 
